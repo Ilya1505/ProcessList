a528adc011e0f040b119141cbce1744188af7c04
Ilya1505 <vesna1505@yandex.ru>
2023-08-17 22:02:47 +0700

    добавлен вывод списка всех процессов в системе

--- ProcessList/Form1.Designer.cs
+++ ProcessList/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProcessList
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gridProcess = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.threads = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridProcess)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // gridProcess
+            // 
+            this.gridProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridProcess.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridProcess.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridProcess.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gridProcess.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridProcess.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.memory,
+            this.time,
+            this.threads});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridProcess.DefaultCellStyle = dataGridViewCellStyle2;
+            this.gridProcess.Location = new System.Drawing.Point(0, 0);
+            this.gridProcess.Name = "gridProcess";
+            this.gridProcess.RowHeadersVisible = false;
+            this.gridProcess.RowHeadersWidth = 51;
+            this.gridProcess.RowTemplate.Height = 24;
+            this.gridProcess.Size = new System.Drawing.Size(1263, 501);
+            this.gridProcess.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Имя";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // memory
+            // 
+            this.memory.HeaderText = "Память (Мб)";
+            this.memory.MinimumWidth = 6;
+            this.memory.Name = "memory";
+            this.memory.ReadOnly = true;
+            // 
+            // time
+            // 
+            this.time.HeaderText = "Время работы (Сек)";
+            this.time.MinimumWidth = 6;
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            // 
+            // threads
+            // 
+            this.threads.HeaderText = "Число потоков";
+            this.threads.MinimumWidth = 6;
+            this.threads.Name = "threads";
+            this.threads.ReadOnly = true;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 523);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1262, 748);
+            this.Controls.Add(this.gridProcess);
+            this.MinimumSize = new System.Drawing.Size(1280, 795);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Список процессов";
+            ((System.ComponentModel.ISupportInitialize)(this.gridProcess)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView gridProcess;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn memory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn threads;
     }
 }
 
--- ProcessList/Form1.cs
+++ ProcessList/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.ComponentModel;
 
 namespace ProcessList
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            //gridProcess.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            gridProcess.AutoResizeRows();
+            PrintProcess();
+        }
+
+        public void PrintProcess()
+        {
+            Process[] localAll = Process.GetProcesses();
+            foreach (Process p in localAll)
+            {
+                string s;
+                try { s = p.StartTime.ToString(); } catch { s = "Отказано в доступе"; }
+                gridProcess.Rows.Add(p.Id.ToString(), p.ProcessName, p.PeakWorkingSet64.ToString(), s, p.Threads.Count.ToString());
+            }
         }
     }
 }
