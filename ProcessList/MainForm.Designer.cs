80427d772f31e56d9d8f30753cd02d349448dd2e
Ilya1505 <vesna1505@yandex.ru>
2023-08-22 14:48:46 +0700

    Добавлена работа с потоками и toolStrip формы

--- ProcessList/MainForm.Designer.cs
+++ ProcessList/MainForm.Designer.cs
@@ -28,17 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.gridProcessList = new System.Windows.Forms.DataGridView();
-            this.labelCountProc = new System.Windows.Forms.Label();
-            this.labelCommonMemory = new System.Windows.Forms.Label();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.memory = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.threads = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelCountProc = new System.Windows.Forms.Label();
+            this.labelCommonMemory = new System.Windows.Forms.Label();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.startButton = new System.Windows.Forms.ToolStripButton();
+            this.aboutProgrammButton = new System.Windows.Forms.ToolStripButton();
+            this.exitButton = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.gridProcessList)).BeginInit();
+            this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // gridProcessList
@@ -48,14 +54,14 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gridProcessList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.gridProcessList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.gridProcessList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridProcessList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.gridProcessList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridProcessList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id,
@@ -63,46 +69,24 @@
             this.memory,
             this.time,
             this.threads});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.gridProcessList.DefaultCellStyle = dataGridViewCellStyle2;
-            this.gridProcessList.Location = new System.Drawing.Point(0, 0);
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridProcessList.DefaultCellStyle = dataGridViewCellStyle4;
+            this.gridProcessList.Location = new System.Drawing.Point(0, 35);
             this.gridProcessList.Name = "gridProcessList";
             this.gridProcessList.RowHeadersVisible = false;
             this.gridProcessList.RowHeadersWidth = 51;
             this.gridProcessList.RowTemplate.Height = 24;
-            this.gridProcessList.Size = new System.Drawing.Size(1263, 630);
+            this.gridProcessList.Size = new System.Drawing.Size(1263, 600);
             this.gridProcessList.TabIndex = 0;
             this.gridProcessList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridProcess_CellContentClick);
             this.gridProcessList.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.gridProcessList_CellMouseDoubleClick);
             this.gridProcessList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gridProcessList_KeyDown);
-            // 
-            // labelCountProc
-            // 
-            this.labelCountProc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.labelCountProc.AutoSize = true;
-            this.labelCountProc.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCountProc.Location = new System.Drawing.Point(12, 640);
-            this.labelCountProc.Name = "labelCountProc";
-            this.labelCountProc.Size = new System.Drawing.Size(293, 38);
-            this.labelCountProc.TabIndex = 1;
-            this.labelCountProc.Text = "Всего процессов: ";
-            // 
-            // labelCommonMemory
-            // 
-            this.labelCommonMemory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.labelCommonMemory.AutoSize = true;
-            this.labelCommonMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCommonMemory.Location = new System.Drawing.Point(12, 688);
-            this.labelCommonMemory.Name = "labelCommonMemory";
-            this.labelCommonMemory.Size = new System.Drawing.Size(262, 38);
-            this.labelCommonMemory.TabIndex = 2;
-            this.labelCommonMemory.Text = "Общая память: ";
             // 
             // id
             // 
@@ -139,11 +123,77 @@
             this.threads.Name = "threads";
             this.threads.ReadOnly = true;
             // 
+            // labelCountProc
+            // 
+            this.labelCountProc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelCountProc.AutoSize = true;
+            this.labelCountProc.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCountProc.Location = new System.Drawing.Point(12, 640);
+            this.labelCountProc.Name = "labelCountProc";
+            this.labelCountProc.Size = new System.Drawing.Size(293, 38);
+            this.labelCountProc.TabIndex = 1;
+            this.labelCountProc.Text = "Всего процессов: ";
+            // 
+            // labelCommonMemory
+            // 
+            this.labelCommonMemory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelCommonMemory.AutoSize = true;
+            this.labelCommonMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCommonMemory.Location = new System.Drawing.Point(12, 688);
+            this.labelCommonMemory.Name = "labelCommonMemory";
+            this.labelCommonMemory.Size = new System.Drawing.Size(262, 38);
+            this.labelCommonMemory.TabIndex = 2;
+            this.labelCommonMemory.Text = "Общая память: ";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startButton,
+            this.aboutProgrammButton,
+            this.exitButton});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(1262, 35);
+            this.toolStrip.TabIndex = 3;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // startButton
+            // 
+            this.startButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(78, 32);
+            this.startButton.Text = "Запуск";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // aboutProgrammButton
+            // 
+            this.aboutProgrammButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.aboutProgrammButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aboutProgrammButton.Image = ((System.Drawing.Image)(resources.GetObject("aboutProgrammButton.Image")));
+            this.aboutProgrammButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.aboutProgrammButton.Name = "aboutProgrammButton";
+            this.aboutProgrammButton.Size = new System.Drawing.Size(140, 32);
+            this.aboutProgrammButton.Text = "О программе";
+            // 
+            // exitButton
+            // 
+            this.exitButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Image = ((System.Drawing.Image)(resources.GetObject("exitButton.Image")));
+            this.exitButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(73, 32);
+            this.exitButton.Text = "Выход";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 748);
+            this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.labelCommonMemory);
             this.Controls.Add(this.labelCountProc);
             this.Controls.Add(this.gridProcessList);
@@ -152,6 +202,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Список процессов";
             ((System.ComponentModel.ISupportInitialize)(this.gridProcessList)).EndInit();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,6 +218,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn memory;
         private System.Windows.Forms.DataGridViewTextBoxColumn time;
         private System.Windows.Forms.DataGridViewTextBoxColumn threads;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton startButton;
+        private System.Windows.Forms.ToolStripButton aboutProgrammButton;
+        private System.Windows.Forms.ToolStripButton exitButton;
     }
 }
 
--- ProcessList/MainForm.cs
+++ ProcessList/MainForm.cs
@@ -5,11 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using System.Diagnostics;
 using NLog;
 using NLog.Fluent;
+using NLog.LayoutRenderers;
 
 namespace ProcessList
 {
@@ -20,30 +21,49 @@ namespace ProcessList
         private string labelCountProcText;
         private string labelCommonMemoryText;
         private double coefMb;
+        private Thread updateThread;
+        private static ManualResetEvent mre;
         public MainForm()
         {
-            logger = LogManager.GetCurrentClassLogger();
             InitializeComponent();
             setup();
-            UpdateProcess();
         }
-
+        ~MainForm()
+        {
+            updateThread.Abort();
+        }
         public void setup()
         {
+            logger = LogManager.GetCurrentClassLogger();
             logger.Info("Инициализация системы");
             coefMb = 1048576;
             gridProcessList.AutoResizeRows();
             gridProcessList.AllowUserToAddRows = false;
             labelCountProcText = "Всего процессов: ";
             labelCommonMemoryText = "Общая память: ";
+            gridProcessList.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            work();
+            mre = new ManualResetEvent(true);
+            updateThread = new Thread(UpdateProcess);
+            updateThread.Start();
+            mre.Reset();
         }
         public void UpdateProcess()
+        {
+            while (updateThread.IsAlive)
+            {
+                work();
+            }
+        }
+        private void work()
         {
             localAll = Process.GetProcesses();
             int time;
             string timeString;
             double memory;
             double totalMemory = 0;
+            DataGridViewRow[] rows = new DataGridViewRow[localAll.Length];
+            int i = 0;
             foreach (Process process in localAll)
             {
                 memory = Math.Round(process.WorkingSet64 / coefMb, 2);
@@ -51,20 +71,41 @@ namespace ProcessList
                 {
                     time = process.TotalProcessorTime.Seconds + process.UserProcessorTime.Seconds;
                     timeString = (time / 60).ToString() + ":" + (time % 60).ToString();
-                } catch {
+                }
+                catch
+                {
                     timeString = "Отказано в доступе";
                 }
-                gridProcessList.Rows.Add(process.Id.ToString(), process.ProcessName, memory.ToString(), timeString, process.Threads.Count.ToString());
+                rows[i] = new DataGridViewRow();
+                rows[i].CreateCells(gridProcessList, process.Id.ToString(), process.ProcessName, memory.ToString(), timeString, process.Threads.Count.ToString());
                 totalMemory += memory;
+                i++;
             }
-            labelCountProc.Text = labelCountProcText + localAll.Length.ToString();
-            labelCommonMemory.Text = labelCommonMemoryText + Math.Round(totalMemory, 2).ToString() + " Мб";
+            if (InvokeRequired)
+            {
+                Action actionClear = () => gridProcessList.Rows.Clear();
+                Action actionAdd = () => gridProcessList.Rows.AddRange(rows);
+                Action actionUpdateLabelCount = () => labelCountProc.Text = labelCountProcText + localAll.Length.ToString();
+                Action actionUpdateLabelMemory = () => labelCommonMemory.Text = labelCommonMemoryText + Math.Round(totalMemory, 2).ToString() + " Мб";
+                BeginInvoke(actionClear);
+                BeginInvoke(actionAdd);
+                BeginInvoke(actionUpdateLabelCount);
+                BeginInvoke(actionUpdateLabelMemory);
+                Thread.Sleep(1000);
+                mre.WaitOne();
+            }
+            else
+            {
+                gridProcessList.Rows.Clear();
+                gridProcessList.Rows.AddRange(rows);
+                labelCountProc.Text = labelCountProcText + localAll.Length.ToString();
+                labelCommonMemory.Text = labelCommonMemoryText + Math.Round(totalMemory, 2).ToString() + " Мб";
+            }
         }
-
         private void gridProcess_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             logger.Info("Выделение строки пользователем");
-            gridProcessList.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            gridProcessList.Rows[e.RowIndex].Selected = true;
         }
 
         private void gridProcessList_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -97,6 +138,22 @@ namespace ProcessList
                     logger.Error("Ошибка открытия формы ProcessInfoForm");
                     MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (toolStrip.Items[0].Text == "Запуск")
+            {
+                logger.Info("Запуск потока на обновление процессов");
+                toolStrip.Items[0].Text = "Стоп";
+                mre.Set();
+            }
+            else
+            {
+                logger.Info("Остановка потока обновления процессов");
+                toolStrip.Items[0].Text = "Запуск";
+                mre.Reset();
             }
         }
     }
